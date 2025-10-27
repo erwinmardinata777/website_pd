321b1b8b5b92f823eeb09efd6778c8dcd95b9347
Erwin Mardinata <erwin.mardinata@gmail.com>
2025-10-27 09:52:01 +0800

    seeder db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            'name' => 'Admin',
+            'email' => 'admin@sumbawakab.go.id',
+            'password' => Hash::make('Sumbawa@1959'),
+        ]);        
     }
 }
